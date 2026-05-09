bfc7d4a1113ffaf83aa4a3743f2d785c0f9decbc
Ho Gia Bao <hogiabao7725@gmail.com>
2026-05-09 16:31:03 +0700

    WIP(register): basic implementation of registration and email activation

---
+++ src/main/java/com/hogiabao7725/financetracker/entity/Profile.java
@@ -0,0 +1,46 @@
+package com.hogiabao7725.financetracker.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Profile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String fullName;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private String profileImageUrl;
+
+    @Column(updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    private Boolean isActive;
+
+    private String activationToken;
+
+    @PrePersist
+    public void prePersist() {
+        if (this.isActive == null) {
+            isActive = false;
+        }
+    }
+}
